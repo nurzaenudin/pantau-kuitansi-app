8860508fb05fd1094d88e605be5cfa58f662da93
Nur Zaenudin <nurzaenudin@gmail.com>
2023-01-31 14:22:30 +0700

    first commit

---
+++ src/views/KuitansiView.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="kuitansi">
+      <h1>Ini adalah kuitansi</h1>
+      <table border="1">
+        <tr>
+            <th>judul 1</th>
+            <th>judul 2</th>
+            <th>judul 3</th>
+            <th>Aksi</th>
+        </tr>
+        
+<tr>
+    <td> isi 11</td>
+    <td> isi 21</td>
+    <td> isi 31</td>
+    <td> Edit Hapus</td>
+</tr>
+<tr>
+    <td> isi 12</td>
+    <td> isi 22</td>
+    <td> isi 32</td>
+    <td> Edit Hapus</td>
+</tr>
+      </table>
+    </div>
+  </template>
+  
+  <style>
+
+  </style>

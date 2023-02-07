eef8f8eaf9a9157b7bac62971c80671138ac0936
Nur Zaenudin <nurzaenudin@gmail.com>
2023-02-07 16:16:33 +0700

    delete berhasil, tabel gagal reactive

--- src/API/PerintahBayar.js
+++ src/API/PerintahBayar.js
@@ -1,6 +1,6 @@
 import { APISetting } from './config.js';
 export default{
-    index(){
+    getAll(){
         return fetch (APISetting.baseURL +'perintahbayar/all',{
             method:'GET',
             headers: APISetting.headers
@@ -14,6 +14,11 @@ export default{
         });
     },
     delete(id){
-        return fetch (APISetting.baseURL + 'perintahbayar/' + id)
+        return fetch (APISetting.baseURL + 'perintahbayar/' + id,{
+            method:'DELETE',
+            headers: APISetting.headers
+        })
+        .then(()=>this.status='Delete Successfull')
+        .then(()=>console.log('hapus Perintah Bayar ID'+ id));
     }
 }
--- src/views/PerintahBayar/ListView.vue
+++ src/views/PerintahBayar/ListView.vue
@@ -5,14 +5,40 @@ import PerintahBayarAPI from '../../API/PerintahBayar.js'
 //reactive state
 //const perintahbayars=ref([]);
 
-const perintahbayars=ref({});
-const getPerintahBayars=async()=>{
-  perintahbayars.value=await PerintahBayarAPI.index();
 
+const perintahbayarsDTO=ref({});
+const perintahbayars=ref({});
+const getPerintahBayarsDTO=async()=>{
+  perintahbayarsDTO.value=await PerintahBayarAPI.getAll();
 };
 
-console.log('ini udin ya');
+function tesKlik(){
+  console.log('ini tes klik');
+}
+
+function hapusPerintahBayar(id){
+  PerintahBayarAPI.delete(id);
+  console.log("ini dari hapus" + perintahbayarsDTO);
+}
+
+function getPerintahBayars(){
+  getPerintahBayarsDTO();
+  
+  perintahbayars.value=perintahbayarsDTO['content'];
+  console.log('dari dalam')
+
+  
+
+}
+  
+
 getPerintahBayars();
+console.log('value doang ');
+console.log(perintahbayars.value);
+console.log('====');
+
+
+
 
 
 
@@ -36,24 +62,20 @@ getPerintahBayars();
             <th>Aksi</th>
         </tr>
         
-<tr v-for="perintahbayar in perintahbayars.content">
+<tr v-for="perintahbayar in perintahbayarsDTO.content" v-bind:key="perintahbayar.id">
     <td> {{perintahbayar.hargaSatuan}}</td>
     <td> {{perintahbayar.keterangan}}</td>
     <td> {{perintahbayar.namaPic}}</td>
     <td> {{perintahbayar.namaRekanan}}</td>
     <td> {{perintahbayar.status}}</td>
-    <td> Edit <RouterLink to="/perintahbayar/form">Hapus {{PerintahBayarAPI}}</RouterLink> {{perintahbayar.id}}</td>
-</tr>
-<tr>
-    <td> isi 1</td>
-    <td> isi 2</td>
-    <td> isi 3</td>
-    <td> Edit Hapus</td>
+    <td> Edit <button @click="hapusPerintahBayar(perintahbayar.id)">Hapus</button></td>
 </tr>
       </table>
     </div>
 
-    SPBYS: {{perintahbayars}}
+    Perintah Bayar DTO : {{perintahbayarsDTO}}
+    Perintah Bayar : {{ perintahbayars }}
+    <button @click="tesKlik">Tes Klik</button>
 
   
   </template>
